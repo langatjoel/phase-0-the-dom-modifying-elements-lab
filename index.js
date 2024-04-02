7ad6dcc724bb4f7d5556311c9476938cba14f572
joel langat <langatjoel42@gmail.com>
2024-04-02 19:02:53 +0300

    made some solutions

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+// Creating a new DOM node
+ const node = document.createElement("div");
+
+ // Set text content 
+ node.textContent = "Joel is the champion";
+
+ document.body.appendChild(node);
